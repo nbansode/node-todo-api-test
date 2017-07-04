e67c66884feafb1ba3feaf76484c3088962584a4
neel bansode <neelb18@gmail.com>
2017-07-04 15:42:03 +0530

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -30,6 +30,16 @@ app.post('/todos', (req, res) => {
   console.log(e);
 });
 
+app.get('/todos', (req, res) => {
+  Todo.find().then((todos) => {
+    res.send({
+      todos
+    })
+  }, (e) => {
+    res.status(400).send(e);
+  });
+});
+
 app.listen(3000, () => {
   console.log('Started on port 3000');
 });
